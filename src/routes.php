e0da6b1d8385bd8eb584deb521248b8cbb31f41a
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-11-30 09:38:52 -0300

    feat: create some routes of project

--- src/routes.php
+++ src/routes.php
@@ -2,3 +2,8 @@
 use core\Router;
 
 $router = new Router();
+
+$router->get('/', 'HomeController@index');
+$router->get('/login', 'LoginController@signin');
+$router->post('/login', 'LoginController@signinAction');
+$router->get('/register', 'LoginController@signup');

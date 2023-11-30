f8dd0d8ad0bcb7f6bd3c1f48e1129063c7408b0d
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-11-30 10:52:59 -0300

    feat: add routes of register user

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,9 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
+
 $router->get('/login', 'LoginController@signin');
 $router->post('/login', 'LoginController@signinAction');
+
 $router->get('/register', 'LoginController@signup');
+$router->post('/register', 'LoginController@signupAction');

9a1c4db8814871a86c63e0f856d0cf729216a708
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-12-04 23:32:50 -0300

    feat: create function of model user

--- src/models/User.php
+++ src/models/User.php
@@ -1,7 +1,64 @@
 <?php
+
 namespace src\models;
+
 use \core\Model;
 
-class User extends Model {
-  
+class User extends Model
+{
+  private $id;
+  private $name;
+  private $email;
+  private $token;
+  private $avatar;
+
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  public function getEmail()
+  {
+    return $this->email;
+  }
+
+  public function setEmail($email)
+  {
+    $this->email = strtolower($email);
+  }
+
+  public function getToken()
+  {
+    return $this->token;
+  }
+
+  public function setToken($token)
+  {
+    $this->token = $token;
+  }
+
+  public function getAvatar()
+  {
+    return $this->avatar;
+  }
+
+  public function setAvatar($avatar)
+  {
+    $this->avatar = $avatar;
+  }
 }

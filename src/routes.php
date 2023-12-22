c33c7bbf810fe9c02f9b5dad2aa7b680daa93a8d
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-12-21 22:42:41 -0300

    feat: routes of profile and logout

--- src/routes.php
+++ src/routes.php
@@ -7,8 +7,12 @@ $router->get('/', 'HomeController@index');
 
 $router->get('/login', 'LoginController@signin');
 $router->post('/login', 'LoginController@signinAction');
+$router->get('/logout', 'LoginController@logout');
 
 $router->get('/register', 'LoginController@signup');
 $router->post('/register', 'LoginController@signupAction');
+
+$router->get('/profile', 'ProfileController@index');
+$router->get('/profile/follow/{id}', 'ProfileController@follow');
 
 $router->post('/post', 'PostController@create');
